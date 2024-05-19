ae29d5f0a6c6488b5c8b44c61f390acaf37765fb
Raspberry Rat <raspberryratman@gmail.com>
2024-05-19 08:53:33 -0600

    added controller methods, need to finish to allow change to admin/poster attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,23 @@ class UsersController < ApplicationController
       redirect_to root_path, alert: "User not found."
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin, :poster)
+  end
 end
