f7a8020db9abd20b53cb37403e714f91075c2e12
squid <raspberryratman@gmail.com>
2024-06-08 18:09:23 -0600

    auto sign out if user is not approved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :sign_out_user_if_pending
 
   protected
   def authenticate_admin!
     authenticate_user!
     redirect_to root_path, status: :forbidden unless current_user.admin?
+  end
+
+  private
+
+  def sign_out_user_if_pending
+    sign_out(current_user) if current_user && !current_user.approve?
   end
 end
