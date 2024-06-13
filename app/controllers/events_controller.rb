fdb0281066f2aa346ce5f1f9183ec1d4c55c5bce
squid <raspberryratman@gmail.com>
2024-06-13 14:42:02 -0600

    added edit and destroy options for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_admin!, only: [:new, :create]
-
+  before_action :authorized_organizer, except: [:index, :show]
   def new
     @event = Event.new
   end
@@ -26,10 +25,34 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path, notice: 'Event successfully deleted'
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:name, :description, :location, :date)
   end
 
+  def authorized_organizer
+    redirect_to root_path, status: :forbidden unless current_user.admin? || current_user.organizer?
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to @post
+      redirect_to @post, notice: 'Post was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
