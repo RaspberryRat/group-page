6be075d3c833e00f8490edc5bafd5ab5f6280288
squid <raspberryratman@gmail.com>
2024-06-15 10:03:57 -0600

    added validation for post title and error messages
    e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_rich_text :content
 
+  validates :title, presence: true, length: { minimum: 5 }
 
   def post_date
     return self.created_at if self.date.nil?
