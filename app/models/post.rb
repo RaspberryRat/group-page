04767992b8151e495a031c215d16bb84721cf58a
Raspberry Rat <raspberryratman@gmail.com>
2024-11-07 14:46:23 -0500

    added dependet destroy so post tag associated is deleted when post is deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_rich_text :content
 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   validates :title, presence: true, length: { minimum: 5 }
